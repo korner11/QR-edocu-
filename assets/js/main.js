a1634d56bc2ffe274ba5b4a5c9b04617ecbcc5d2
korner11 <m.drahovsky@gmail.com>
2016-09-09 15:19:11 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,34 +5,43 @@ angular.module('app', [])
     $scope.showForm = false;
     $scope.showHash = true;
     $scope.hash =  $location.path().substring(1,$location.path.toString().length);
-    //$scope.hash = $window.location.hash.substring(1,$window.location.hash.length);
-    var getUrl= 'http://localhost:8383/getqr/' + $scope.hash;
-    
-    $scope.getQR= function (){  
-        $http({
-               method: 'GET',
-               url: getUrl
-               }).then(function successCallback(response) {
-                        $scope.message= "Zadany QR kód má tento hash = " + $scope.hash;
-               }, function errorCallback(response) {
-                        $scope.message= "Zadany QR kód sa nepodarilo nájsť";
-               });
+    console.log($scope.hash);
+        //$scope.hash = $window.location.hash.substring(1,$window.location.hash.length);
+    if($scope.hash != ""){
+            var getUrl= 'http://localhost:8383/getqr/' + $scope.hash;
+
+            $scope.getQR= function (){  
+                $http({
+                       method: 'GET',
+                       url: getUrl
+                       }).then(function successCallback(response) {
+                                $scope.message= "QR code hash is = " + $scope.hash;
+                       }, function errorCallback(response) {
+                                $scope.message= "Sorry! Couldn't find QR code";
+                       });
+            }
+    }else{
+      $scope.message = "Please insert QR code"  
     }
     $scope.pairElmnt= function (){
-        var postUrl= 'http://localhost:8383/pairqr/' + $scope.hash;
-         $http({
-                method: 'POST',
-                url: postUrl,
-                data: {url: 'http://google.com'}
-                }).then(function successCallback(response) {
-                        $scope.message= "Zadany QR kód bol pridaný";
-                }, function errorCallback(response) {
-                    $scope.message= "Nepodarilo sa spárovať dáný QR kód";
-                });
-
+         if($scope.hash != ""){
+                var postUrl= 'http://localhost:8383/pairqr/' + $scope.hash;
+                $http({
+                       method: 'POST',
+                       url: postUrl,
+                       data: {url: 'http://google.com'}
+                       }).then(function successCallback(response) {
+                               $scope.message= "QR code was paired";
+                       }, function errorCallback(response) {
+                           $scope.message= "Sorry! Couldn't   find or pair QR code";
+                       });
+        }else{
+             $scope.message = "Please insert QR code"  
+        }        
     }
     
     $scope.newElmnt= function(){
+            
             $scope.showForm= true;
             $scope.showHash = false;
              if (navigator.geolocation) {
@@ -42,23 +51,28 @@ angular.module('app', [])
                           });
                  });
             }else{
-                $scope.location="Nepodporuje geolokaciu";
+                $scope.location="Not supported geolocation";
             }
     }
     
     $scope.sendForm = function(){
-        var postUrl= 'http://localhost:8383/newqr/' + $scope.hash;
-        if($scope.name != null && $scope.description !=null){
-            $http({
-                   method: 'POST',
-                   url: postUrl,
-                   data: {name: $scope.name, description: $scope.description, location: $scope.location}
-                   }).then(function successCallback(response) {
-                       $scope.message2= "Zadany QR kód bol vytvorený";
-                   }, function errorCallback(response) {
-                       $scope.message2= "Nepodarilo sa pridať dáný QR kód";
-                   });
-        }
+       
+        if($scope.hash != ""){
+            var postUrl= 'http://localhost:8383/newqr/' + $scope.hash;
+            if($scope.name != null && $scope.description !=null){
+                $http({
+                       method: 'POST',
+                       url: postUrl,
+                       data: {name: $scope.name, description: $scope.description, location: $scope.location}
+                       }).then(function successCallback(response) {
+                           $scope.message2= "QR code was created";
+                       }, function errorCallback(response) {
+                           $scope.message2= "Sorry! Couldn't create QR code";
+                       });
+            }
+        }else{
+             $scope.message2 = "Please insert QR code"  
+          }  
     }
 
 }]);
